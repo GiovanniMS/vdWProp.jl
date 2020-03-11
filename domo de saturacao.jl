6d5aa8d051ee68f46dce4a187973c048582edc7e
Giovanni de Medeiros Silvestri <gimsilvestri@gmail.com>
2020-03-11 15:00:13 -0300

    plots Pr x sr , Pr x ur, Pr x hr

--- domo de saturacao.jl
+++ domo de saturacao.jl
@@ -32,7 +32,9 @@ Pr_sat(V1,V2) = (27*(-V1 - V2 + (V1 + 1)*(V2 + 1) - 2)) / (((V1 + 1)^2)*((V2 + 1
 
 #ARRAYS
 
-℘list = range(0.0001, stop = 0.95, length = 10000)
+points = 10000
+
+℘list = range(0.0001, stop = 0.95, length = points)
 
 V2list = []
 
@@ -70,7 +72,7 @@ for i in ℘list
     
 end
 
-#######################################################################################
+########################################################################################
 
 #PLOT Pxv
 
@@ -105,3 +107,114 @@ scene = lines(x1, y, color = :blue)
 lines!(scene, x2, y, color = :green)
 
 display(scene)
+
+########################################################################################
+
+# utilizando as propriedades do domo podemos encontrar as demais propriedades associadas
+# a partir das demais propriedades, havera variacao dependendo do tipo de gas (por causa do ϕ) e dependendo das constantes escolhidas
+# portanto deve-se escolher as constantes e fazer um domo para cada tipo de gas (3 domos)
+
+Zc = 3/8
+
+ar(vr,Tr,ϕ,C1) = C1 + Tr*(C2 - (ϕ/Zc)*log(Tr) + ϕ/Zc) - (8/3)*Tr*log(3vr - 1) - (3/vr)
+
+sr(vr,Tr,ϕ,C2) = (8/3)*((log(3vr - 1)) + ϕ*log(Tr)) - C2
+
+ur(vr,Tr,ϕ,C1) = C1 + (8/3)*Tr*ϕ - (3/vr)
+
+hr(vr,Tr,ϕ,C1) = C1 + (8/3)*Tr*ϕ + ((8*Tr*vr)/(3vr - 1)) - (6/vr)
+
+# inicialmente serao considerados C1 = C2 = 0
+# caso 1 - ϕ = 3/2
+# caso 2 - ϕ = 5/2
+# caso 3 - ϕ = 7/2
+
+ϕ1 = 3/2
+
+ϕ2 = 5/2
+
+ϕ3= 7/2
+
+C=0
+
+# Funcao para obter as propriedades e plots em todos os casos
+
+function domoprop(ϕ::Number,C1::Number,C2::Number)
+    
+    if ϕ == ϕ1 || ϕ == ϕ2 || ϕ == ϕ3
+
+        sr1list = []
+
+        ur1list = []
+
+        hr1list = []
+
+        sr2list = []
+
+        ur2list = []
+
+        hr2list = []
+
+        for i in 1:points
+
+            append!(sr1list,Float64(sr(vr1list[i],Tr_sat_list[i],ϕ,C2)))
+
+            append!(ur1list,Float64(ur(vr1list[i],Tr_sat_list[i],ϕ,C1)))
+
+            append!(hr1list,Float64(hr(vr1list[i],Tr_sat_list[i],ϕ,C1)))
+
+        end
+
+        for i in 1:points
+
+            append!(sr2list,Float64(sr(vr2list[i],Tr_sat_list[i],ϕ,C2)))
+
+            append!(ur2list,Float64(ur(vr2list[i],Tr_sat_list[i],ϕ,C1)))
+
+            append!(hr2list,Float64(hr(vr2list[i],Tr_sat_list[i],ϕ,C1)))
+
+        end
+
+        x1 = Array{Float64,1}(sr1list)
+
+        x2 = Array{Float64,1}(sr2list)
+
+        y = Array{Float64,1}(Pr_sat_list)
+
+        scene = lines(x1, y, color = :blue)
+
+        lines!(scene, x2, y, color = :green)
+
+        Makie.save("Pr x sr.jpg", scene)
+
+        x1 = Array{Float64,1}(ur1list)
+
+        x2 = Array{Float64,1}(ur2list)
+
+        y = Array{Float64,1}(Pr_sat_list)
+
+        scene = lines(x1, y, color = :blue)
+
+        lines!(scene, x2, y, color = :green)
+
+        Makie.save("Pr x ur.jpg", scene)
+
+        x1 = Array{Float64,1}(hr1list)
+
+        x2 = Array{Float64,1}(hr2list)
+
+        y = Array{Float64,1}(Pr_sat_list)
+
+        scene = lines(x1, y, color = :blue)
+
+        lines!(scene, x2, y, color = :green)
+
+        Makie.save("Pr x hr.jpg", scene)
+        
+    else 
+        
+        print("ERROR, ϕ needs to be 3/2, 5/2 or 7/2")
+        
+    end
+        
+end
